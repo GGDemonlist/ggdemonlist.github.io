9a7314473dc2188cb5fc43747eb742ee3840ee55
GGDemonlist <lilo35382@gmail.com>
2024-05-15 19:50:01 +0300

    Update Form.js

--- js/pages/Form.js
+++ js/pages/Form.js
@@ -14,7 +14,7 @@ export default {
         <div align="center"><h2>
         
         </h2>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeQ8gTtEZopyL2P8Ck0pYtiYciylaNia1PXowUDYgz0KyXUzQ/viewform?embedded=true" width="450" height="1000" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeQ8gTtEZopyL2P8Ck0pYtiYciylaNia1PXowUDYgz0KyXUzQ/viewform?embedded=true" width="450" height="900" frameborder="0" marginheight="0" marginwidth="0">Загрузка…</iframe>
         <div>
         </main> 
     `,
